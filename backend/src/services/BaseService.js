11d07dfa9159d0645881257ca5a14647ecd87282
mori32111 <gafeltran982@gmail.com>
2026-08-05 22:30:51 -0300

    CRUD básico no BaseService.js

--- backend/src/services/BaseService.js
+++ backend/src/services/BaseService.js
@@ -8,4 +8,29 @@ export default class BaseService {
     async listar(){
         return await this.model.findMany();
     }
+
+    async buscarPorId(id) {
+        return await this.model.findUnique({
+            where: { id }
+        });
+    }
+
+    async criar(dados) {
+        return await this.model.create({
+            data: dados
+        });
+    }
+
+    async atualizar(id, dados) {
+        return await this.model.update({
+            where: { id },
+            data: dados
+        });
+    }
+
+    async excluir(id) {
+        return await this.model.delete({
+            where: { id }
+        });
+    }
 }
