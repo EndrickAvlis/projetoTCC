a6c027d5576b8df3eeba942e246f033e4d59c0d7
Endrick <endrick98814515@gmail.com>
2026-08-21 20:23:02 -0300

    documentação da tela de alunos

--- frontend/src/features/admin/services/ProdutosService.js
+++ frontend/src/features/admin/services/ProdutosService.js
@@ -61,19 +61,19 @@ export const alterarEstoqueUniforme = (produtoId, alteracao) =>
     body: JSON.stringify(alteracao),
   });
 
+// export const buscarConfiguracaoArmario = () =>
+//   Promise.resolve({
+//     produto: {
+//       id: 20,
+//       nome: "Armário",
+//       preco: 120,
+//       quantidade: 18,
+//       tipo: "armario",
+//       status: "disponivel",
+//     },
+//   });
 export const buscarConfiguracaoArmario = () =>
-  Promise.resolve({
-    produto: {
-      id: 20,
-      nome: "Armário",
-      preco: 120,
-      quantidade: 18,
-      tipo: "armario",
-      status: "disponivel",
-    },
-  });
-//export const buscarConfiguracaoArmario = () =>
-//requisitarApi("/admin/produtos/armario");
+requisitarApi("/admin/produtos/armario");
 
 export const atualizarConfiguracaoArmario = (produtoId, dados) =>
   requisitarApi(criarUrlProduto(produtoId), {
